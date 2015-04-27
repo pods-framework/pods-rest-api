ed9ac08ce1336e1f073c23c309b7ee0aaf589780
Josh Pollock <jpollock412@gmail.com>
2015-04-27 16:34:55 -0400

    include functions.php as well #36

--- bootstrap.php
+++ bootstrap.php
@@ -6,13 +6,12 @@
  * @license   GPL-2.0+
  * @copyright 2015 Pods Framework
  */
+
 include_once( dirname( __FILE__ ) . '/includes/autoloader.php' );
 $loader = new pods_rest_api_autoloader();
-
-
-
 $loader->addNamespace('pods_rest_api', dirname( __FILE__ ) . '/classes' );
-
 $loader->register();
 
 new \pods_rest_api\main();
+
+include_once( dirname( __FILE__ ) . '/includes/functions.php' );
