7c40e6e42b523864ff3d6083b5ac06a56b5a9e5c
Quasel <bernhard@gronau.at>
2015-04-27 12:45:50 +0200

    Revert "Revert ce92a03..94cf9da"
    This reverts commit 2349d1218831b4c49b616409366ee7c12908ede8.

--- classes/class-pods-rest-api.php
+++ classes/class-pods-rest-api.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Main Class for Pods REST API
  *
@@ -6,49 +7,51 @@
  * @license   GPL-2.0+
  * @copyright 2015 Pods Framework
  */
+class Pods_REST_API
+{
 
-class Pods_REST_API {
+    public function __construct()
+    {
+        add_action('wp_json_init', array($this, 'default_routes'));
+    }
 
-	public function __construct() {
-		add_action( 'wp_json_init', array( $this, 'default_routes' ) );
-	}
+    /**
+     * Include the default routes
+     *
+     * @uses "wp_json_server_before_serve" action
+     *
+     * @since 0.0.1
+     */
+    public function default_routes()
+    {
+        if (PODS_REST_API_ENABLE_DEFAULT_ROUTES) {
+            include_once(dirname(__FILE__) . '/routes/class-pods-rest-api-route-pods.php');
+            include_once(dirname(__FILE__) . '/routes/class-pods-rest-api-route-podsapi.php');
+        }
 
-	/**
-	 * Include the default routes
-	 *
-	 * @uses "wp_json_server_before_serve" action
-	 *
-	 * @since 0.0.1
-	 */
-	public function default_routes() {
-		if ( PODS_REST_API_ENABLE_DEFAULT_ROUTES ) {
-			include_once( dirname( __FILE__ ) .'/routes/class-pods-rest-api-route-pods.php' );
-			include_once( dirname( __FILE__ ) .'/routes/class-pods-rest-api-route-podsapi.php' );
-		}
+        /**
+         * Register default JSON API routes
+         */
 
-		/**
-		 * Register default JSON API routes
-		 */
+        $pods_config = pods_api()->load_pods(array('key_names' => true));
 
-		$pods_config = pods_api()->load_pods( array( 'key_names' => true ) );
+        //todo creat a list of default_routes&filter to iterate on (PODs&PodsAPI )
+        foreach ($pods_config as $pod => $config) {
 
-		//todo creat a list of default_routes&filter to iterate on (PODs&PodsAPI )
-		foreach ( $pods_config as $pod => $config  ) {
+            // @todo if ( $config['type'] )  use different controller for pod, post-type, ....
+            // @todo get class from default_routes  (see _add_extra_api_post_type_arguments)
 
-			// @todo if ( $config['type'] )  use different controller for pod, post-type, .... 
-			// @todo get class from default_routes  (see _add_extra_api_post_type_arguments)
+            $class = 'Pods_REST_API_Route_Pods';
 
-			$class = 'Pods_REST_API_Route_Pods';
+            if (!class_exists($class)) {
+                // continue;
+            }
 
-			if ( ! class_exists( $class ) ) {
-			// continue;
-			}
+            $controller = new $class($pod);
+            $controller->register_routes();
 
-			$controller = new $class( $pod );
-			$controller->register_routes();
+        }
 
-		}
-
-	}
+    }
 
 }
--- classes/routes/class-pods-rest-api-route-pods.php
+++ classes/routes/class-pods-rest-api-route-pods.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Handles the PodsAPI class endpoint and routes
  *
@@ -6,100 +7,103 @@
  * @license   GPL-2.0+
  * @copyright 2015 Josh Pollock
  */
+class Pods_REST_API_Route_Pods extends Pods_REST_API_Controller
+{
 
-class Pods_REST_API_Route_Pods extends Pods_REST_API_Controller {
+    /**
+     * URL for this route
+     *
+     * NOTE: Relative to root URL for WordPress' REST API
+     *
+     * @since 0.0.1
+     *
+     * @access protected
+     */
+    protected $pod;
 
-	/**
-	 * URL for this route
-	 *
-	 * NOTE: Relative to root URL for WordPress' REST API
-	 *
-	 * @since 0.0.1
-	 *
-	 * @access protected
-	 */
-	protected $pod;
+    /**
+     * Constructor for parent class.
+     *
+     * @param string $route Route name
+     *
+     * @since 0.0.1
+     */
+    public function __construct($pod)
+    {
+        $this->pod = $pod;
+    }
 
-	/**
-	 * Constructor for parent class.
-	 *
-	 * @param string $route Route name
-	 *
-	 * @since 0.0.1
-	 */
-	public function __construct( $pod ) {
-		$this->pod = $pod;
-	}
+    public function register_routes()
+    {
+        $pod = $this->pod;
+        register_json_route('pods', $pod,
+            array(
+                array(
+                    'methods' => WP_JSON_Server::READABLE,
+                    'callback' => array($this, 'get_items'),
+                    'args' => array(
+                        'context' => array(
+                            'default' => 'view',
+                        ),
+                        'type' => array(),
+                        'page' => array(),
+                    ),
+                    'permission_callback' => array($this, 'permissions_check')
+                )
 
-	public function register_routes() {
-		$pod = $this->pod;
-			register_json_route( 'pods', $pod,
-				array(
-					array(
-						'methods'             => WP_JSON_Server::READABLE,
-						'callback'            => array( $this, 'get_items' ),
-						'args'                => array(
-							'context' => array(
-								'default' => 'view',
-							),
-							'type'    => array(),
-							'page'    => array(),
-						),
-						'permission_callback' => array( $this, 'permissions_check' )
-					)
+            )
 
-				)
+        );
 
-			);
-
-			register_json_route( PODS_REST_API_BASE_URL, '/' . $pod . '/(?P<id>[\d]+)',
-				array (
-					array(
-						'methods'         => WP_JSON_Server::READABLE,
-						'callback'        => array( $this, 'get_item' ),
-						'permission_callback' => array( $this, 'permissions_check' ),
-						'args'            => array(
-							'context'          => array(
-								'default'      => 'view'
-							)
-						)
-					)
-				)
-			);
-		}
-
-
-	public function get_items( $request ) {
-		$args = (array) $request->get_params();
-		$args = pods_sanitize( $args );
-		/**
-		 * Alter the query arguments for a request.
-		 *
-		 * This allows you to set extra arguments or defaults for a post
-		 * collection request.
-		 *
-		 * @param array $args Map of query var to query value.
-		 * @param WP_JSON_Request $request Full details about the request.
-		 */
-		$args = apply_filters( 'json_pods_query', $args, $request );
-		// $query_args = $this->prepare_items_query( $args );
+        register_json_route(PODS_REST_API_BASE_URL, '/' . $pod . '/(?P<id>[\d]+)',
+            array(
+                array(
+                    'methods' => WP_JSON_Server::READABLE,
+                    'callback' => array($this, 'get_item'),
+                    'permission_callback' => array($this, 'permissions_check'),
+                    'args' => array(
+                        'context' => array(
+                            'default' => 'view'
+                        )
+                    )
+                )
+            )
+        );
+    }
 
 
-		$params = array(
-				'depth'  => 2,
-				'limit' => 15,
-				'params' => $args,
-			);
+    public function get_items($request)
+    {
+        $args = (array)$request->get_params();
+        $args = pods_sanitize($args);
+        /**
+         * Alter the query arguments for a request.
+         *
+         * This allows you to set extra arguments or defaults for a post
+         * collection request.
+         *
+         * @param array $args Map of query var to query value.
+         * @param WP_JSON_Request $request Full details about the request.
+         */
+        $args = apply_filters('json_pods_query', $args, $request);
+        // $query_args = $this->prepare_items_query( $args );
 
-		$items = pods_api()->export( $this->pod, $params );
 
-		$items['query_args'] = $params;
-		$items['pod'] = $this->pod;
+        $params = array(
+            'depth' => 2,
+            'limit' => 15,
+            'params' => $args,
+        );
+
+        $items = pods_api()->export($this->pod, $params);
+
+        $items['query_args'] = $params;
+        $items['pod'] = $this->pod;
 
 
-		$response = json_ensure_response( $items );
+        $response = json_ensure_response($items);
 
-		return $response;
-	}
+        return $response;
+    }
 
 }
